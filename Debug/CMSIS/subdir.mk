f6d7fde228eedca1ef64d811bf4dd918861ff870
xstupi00@github.com <xstupi00@github.com>
2018-12-20 00:36:24 +0100

    Add final version

---
+++ Debug/CMSIS/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../CMSIS/system_MK60D10.c 
+
+OBJS += \
+./CMSIS/system_MK60D10.o 
+
+C_DEPS += \
+./CMSIS/system_MK60D10.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+CMSIS/%.o: ../CMSIS/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DSDK_OS_BAREMETAL -DFSL_RTOS_BM -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DCPU_MK60DN512VMD10_cm4 -DCPU_MK60DN512VMD10 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I../board -I../source -I../ -I../drivers -I../CMSIS -I../utilities -I../startup -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m4 -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
